1adb6e2e0a3dbf66fa02fe6af207805d6942a822
Nahuel Fernandez Beschtedt <lnahuelfernandezb@gmail.com>
2020-11-29 09:58:54 -0300

    a

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ import NotFound from "../pages/NotFound";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <Layout>
         <Switch>
           <Route exact path="/" component={Home} />
